2d390f2cbcf8388b2e867d4c8a20a85ed6d24058
Hisham-Shakeel <162502024+Hisham-Shakeel@users.noreply.github.com>
2024-03-14 10:30:02 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+// Add an Event Listener to the button to run the placeOrder function
+document.getElementById("btn").addEventListener("click", BlessingIn);
+document.getElementById("btn2").addEventListener("click", dime);
+document.getElementById("btn3").addEventListener("click", late);
+document.getElementById("btn4").addEventListener("click", pain);
+document.getElementById("btn5").addEventListener("click", file);
+
+// Store order code in a function
+function BlessingIn() {
+    let msg = `disguise`;
+    alert(msg);
+}
+
+function dime() {
+    let msg = `dozen`;
+    alert(msg);
+}
+
+function late() {
+    let msg = `never`;
+    alert(msg);
+}
+
+function pain() {
+    let msg = `No Gain`;
+    alert(msg);
+}
+
+function file() {
+    let msg = `when you're having fun.`;
+    alert(msg);
+}
